4a5989ce5cb4321a1070f43c0da8b27aaf9f673e
Gakarot <startradingcentre@gmail.com>
2025-05-12 15:45:07 +0530

    Update code.sol

--- Challenge_9_VaultUnlock/code.sol
+++ Challenge_9_VaultUnlock/code.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Solution:
+// Use foundry cast
+// cast storage "target Contract address" 1 \
+  --rpc-url https://eth-sepolia.g.alchemy.com/v2/YOUR_ALCHEMY_KEY
+// This will read the storage slot 1 which consist of password.
+// Dont use this trick for unethical means.
+
+
 contract Vault {
     bool public locked;
     bytes32 private password;
