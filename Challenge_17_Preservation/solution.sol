89eb4b3280aec1a0ed95e70a40aee696239b95a4
Gakarot <startradingcentre@gmail.com>
2025-05-14 14:14:37 +0530

    Update solution.sol

--- Challenge_17_Preservation/solution.sol
+++ Challenge_17_Preservation/solution.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 // Call setFirstTime(uint256(address of PreservationAttack)) → this overwrites timeZone1Library
 // Call setFirstTime(uint256(uint160(yourAddress))) again → delegatecall into your contract → owner gets overwritten
 // Direct conversion of address into uint256 is not possible so you can do that offchain like uint256(uint160(address)).
+// Browser console might give a big number error. Easiest solution is to write a script in python/js/solidity for interacting
+//  with the Preservation contract. Then big number error will be handled internally and you can just pass a uint256 number from your script.
 
 contract PreservationAttack {
 
