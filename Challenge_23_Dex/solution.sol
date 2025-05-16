fc17efb19fbe87c730527efdac34b3d316c06448
Gakarot <startradingcentre@gmail.com>
2025-05-16 17:37:30 +0530

    Update solution.sol

--- Challenge_23_Dex/solution.sol
+++ Challenge_23_Dex/solution.sol
@@ -2,15 +2,15 @@
 // We are exploiting getSwapPrice() as there is mathematical error in calculating price.
 /*
 .....token1...token2......
-10in...|..100..|..100..|.10 out
-24out..|..110..|..90...|.20 in
-24in...|...86..|..110..|.30 out
-41out..|..110..|..80...|.30 in
-41in...|..69...|..110..|.65 out
-158out.|..110..|..45...|65 in ----> This will show error coz contract balance is low now so we will just transfer 45 only.
-.......|..-48..|..110..|.... ---> Contract balance cant be in negative.
-110out.|..110..|..45...|.45 in(20 left in user wallet)
-.......|...0...|..90...|....
+10in...|..100..|..100..|..10out
+24out..|..110..|..90...|..20in
+24in...|...86..|..110..|..30out
+41out..|..110..|..80...|..30in
+41in...|..69...|..110..|..65out
+158out.|..110..|..45...|..65in ----> This will show error coz contract balance is low now so we will just transfer 45 only.
+.......|..-48..|..110..|...... ----> Contract balance cant be in negative.
+110out.|..110..|..45...|..45in ----> (20 left in user wallet)
+.......|...0...|..90...|......
 Final balance User - Token 1 - 110 , Token 2 - 20
 Final balance contract - Token 1 - 0 , Token 2 - 90
 */
