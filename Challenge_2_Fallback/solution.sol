ed496f9c92c3948c68dc0787ef26df7aaf720963
Gakarot <startradingcentre@gmail.com>
2025-05-20 20:39:09 +0530

    Update solution.sol

--- Challenge_2_Fallback/solution.sol
+++ Challenge_2_Fallback/solution.sol
@@ -1,1 +1,7 @@
-
+/*
+Solution:
+We will be exploiting the ownership.
+First call contribute() so that we get added in balances.
+Then send some eth while triggering the receive function of the target which has exploit.
+Done you became the onwer.
+*/
